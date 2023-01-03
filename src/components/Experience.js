217b03deb26ff5c7f17001a654576176ff97cda7
Eddie Wang <eddiewang12345@gmail.com>
2023-01-03 01:56:30 -0500

    blender changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,17 +2,26 @@ import {
   ContactShadows,
   Environment,
   Gltf,
+  Line,
   OrbitControls,
+  Segment,
+  SegmentObject,
+  Segments,
   Shadow,
   useGLTF,
 } from '@react-three/drei';
+import {
+  Bloom,
+  EffectComposer,
+  SelectiveBloom,
+} from '@react-three/postprocessing';
 import { Perf } from 'r3f-perf';
 import { Suspense } from 'react';
 
 export default function Experience() {
   console.log('Render');
 
-  const { nodes, materials } = useGLTF('./main.glb');
+  const { nodes, materials } = useGLTF('./line.glb');
 
   console.log(nodes, materials);
 
@@ -21,19 +30,88 @@ export default function Experience() {
       <Perf position='top-left' />
       <color attach={'background'} args={['#111']} />
 
+      <EffectComposer>
+        <Bloom
+          mipmapBlur
+          // luminanceThreshold={0.1}
+          luminanceSmoothing={0}
+          intensity={1}
+        />
+      </EffectComposer>
+
       <OrbitControls makeDefault />
 
-      {/* <ambientLight intensity={10} /> */}
-      <Environment background blur={0.5} files='night.hdr' />
-      <spotLight
+      {/* <ambientLight intensity={1} /> */}
+      {/* <Environment background blur={0.5} files='night.hdr' /> */}
+      {/* <spotLight
         intensity={0.5}
         angle={0.1}
         penumbra={1}
         position={[20, 50, 20]}
         castShadow
-      />
+      /> */}
 
-      <Suspense
+      {/* <Line
+        ref={(obj) => {
+          obj && (obj.material.toneMapped = false);
+        }}
+        lineWidth={1}
+        color={[1, 1, 1]}
+        points={[...nodes.BezierCurve.geometry.attributes.position.array]}
+        vertexColors={[
+          [0, 0, 0],
+          [2, 0, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [1, 1, 0],
+          [0, 0, 20],
+          [0, 0, 20],
+          [0, 0, 20],
+        ]}
+      /> */}
+
+      <line geometry={nodes.BezierCurve.geometry}>
+        <lineBasicMaterial
+          ref={(obj) => console.log(obj)}
+          color={[10, 10, 10]}
+          linewidth={10}
+          needsUpdate={true}
+          toneMapped={false}
+          vertexColors={[
+            [0, 0, 0],
+            [0, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0],
+          ]}
+        />
+      </line>
+
+      {/* <mesh>
+        <boxGeometry />
+        <meshStandardMaterial
+          emissive={'red'}
+          // emissiveIntensity={4}
+          toneMapped={false}
+        />
+      </mesh> */}
+
+      {/* <Suspense
         fallback={
           <mesh>
             <boxBufferGeometry />
@@ -41,16 +119,8 @@ export default function Experience() {
           </mesh>
         }
       >
-        {/* <Gltf src='./Cyber.glb' /> */}
-        {/* <primitive object={model.scene} receiveShadow castShadow /> */}
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes['1stFloot'].geometry}
-          material={materials['']}
-          material-envMapIntensity={0.2}
-        />
-      </Suspense>
+        <primitive object={model.scene} receiveShadow castShadow />
+      </Suspense> */}
 
       <mesh receiveShadow rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry args={[100, 100]} />
